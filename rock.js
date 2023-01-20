d896cefed81f19134e6a5b6af520a9608d90b20a
Karl Schabas <karl.schabas@utoronto.ca>
2023-01-20 14:13:38 -0500

    Create playRound function and test

--- rock.js
+++ rock.js
@@ -15,7 +15,52 @@ function getComputerChoice() {
     }
 }
 
-for (i=0;i<5;i++)
-{
-    console.log(getComputerChoice());
+function playRound(playerSelection, computerSelection) {
+    var player = playerSelection.toLowerCase();
+    var comp = computerSelection.toLowerCase();
+
+    var winner = -1;
+    if (player == "rock") {
+        if (comp=="rock") {
+            winner = 2;
+        }
+        else if (comp=="paper") {
+            winner = 1;
+        }
+        else {
+            winner = 0;
+        }
+    }
+    else if (player == "paper") {
+        if (comp == "paper") {
+            winner = 2;
+        }
+        else if (comp == "scissors") {
+            winner = 1;
+        }
+        else {
+            winner = 0;
+        }
+    }
+    else {
+        if (comp == "scissors") {
+            winner = 2;
+        }
+        else if (comp == "rock") {
+            winner = 1;
+        }
+        else {
+            winner = 0;
+        }
+    }
+
+    if (winner == 0) {
+        return `You Win! ${player} beats ${comp}`;
+    }
+    if (winner == 1) {
+        return `You Lose! ${comp} beats ${player}`;
+    }
+    return `You Tie! Both players chose ${player}`;
 }
+
+console.log(playRound("rock","rock"));
